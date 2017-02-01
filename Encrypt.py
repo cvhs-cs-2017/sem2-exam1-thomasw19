e3f64a851fe275067eaee80306f596cb2841dda4
Trump Winegar <winegath090999@cvh.rusd.us>
2017-02-01 11:29:27 -0800

    done

--- Encrypt.py
+++ Encrypt.py
@@ -8,19 +8,19 @@ def encrypt(AnyString):
     newString = ""
     for ch in AnyString:
         if ord(ch) is 65 or ord(ch) is 97:
-            newString = newString + "%"
+            newString = newString
 
         elif ord(ch) is 69 or ord(ch) is 101:
-            newString = newString + "*"
+            newString = newString 
 
         elif ord(ch) is 73 or ord(ch) is 105:
-            newString = newString + "&"
+            newString = newString
 
         elif ord(ch) is 79 or ord(ch) is 111:
-            newString = newString + "@"
+            newString = newString
 
         elif ord(ch) is 85 or ord(ch) is 117:
-            newString = newString + "$"
+            newString = newString
 
 
         else:
@@ -33,3 +33,29 @@ print (NoVowels)
 
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
+def final(AnyString):
+    newString = ""
+    for ch in AnyString:
+        if ord(ch) is 77 or ord(ch) is 109:
+            newString = newString
+
+        elif ord(ch) is 87 or ord(ch) is 119:
+            newString = newString
+
+        elif ord(ch) is 71 or ord(ch) is 103:
+            newString = newString
+
+        elif ord(ch) is 82 or ord(ch) is 114:
+            newString = newString
+
+        elif ord(ch) is 78 or ord(ch) is 110:
+            newString = newString
+
+
+        else:
+            newString = newString + ch
+
+    return newString
+String ='Computer Science Makes the World go round but it doesn\'t make the world round itself!'
+code  = final (NoVowels)
+print (code)
