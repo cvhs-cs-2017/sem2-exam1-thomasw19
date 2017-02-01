cac233360b026cd21d5f63ba5cd2bd1f9074112c
Trump Winegar <winegath090999@cvh.rusd.us>
2017-02-01 11:07:42 -0800

    3

---
+++ MyFile.py
@@ -0,0 +1,26 @@
+import turle
+thomas = turle
+def AddTen(n):
+    n = n + 10
+    return n
+print ()
+
+
+l = 14
+w = 20
+def DrawRectangle(thomas,l, w):
+    for i in range (4):
+        thomas.forward(14)
+        thomas.right(90)
+        thomas.forward(w)
+DrawRectangle (thomas, l, w)
+
+
+def DrawPoly(exam, n):
+    for i in range(12):
+
+        thomas.forward(34)
+        thomas.right(30)
+
+DrawPoly (thomas,8)
+input()            
